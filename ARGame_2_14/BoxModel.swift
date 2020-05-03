bc134a7fa8f426f73e0f00cbcc8aede46967e992
laurayesayan <laurayesayan@gmail.com>
2020-05-03 23:43:22 +0300

    Added panel with color buttons and action for them.

--- ARGame_2_14/BoxModel.swift
+++ ARGame_2_14/BoxModel.swift
@@ -26,16 +26,16 @@ class BoxModel {
         return SCNVector3(x: Float.random(in: -1...1), y: Float.random(in: -1...1), z: Float.random(in: -1...1))
     }
     
-     func generateRandomColor() -> UIColor {
-        let colors = [0: #colorLiteral(red: 1, green: 0.9598860748, blue: 0.4043213171, alpha: 0.9), 1: #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 0.9), 2: #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 0.9), 3: #colorLiteral(red: 0.1411764771, green: 0.3960784376, blue: 0.5647059083, alpha: 0.9), 4: #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 0.9), 5: #colorLiteral(red: 0.6067150242, green: 1, blue: 0.9936729992, alpha: 0.9)]
-        return colors[Int.random(in: 0...5)]!
+    private func generateRandomColor(_ colorRange: [Int: UIColor]) -> UIColor {
+
+        return colorRange[Int.random(in: 0...5)]!
     }
     
-    func generateBoxes(boxesCount: Int) -> [SCNNode] {
+    func generateBoxNodes(boxesCount: Int, colorRange: [Int: UIColor]) -> [SCNNode] {
         var boxes: [SCNNode] = []
         for _ in 0..<boxesCount {
             let boxPosition = generateRandomPosition()
-            let boxColor = generateRandomColor()
+            let boxColor = generateRandomColor(colorRange)
             boxes.append(createBox(position: boxPosition, color: boxColor))
         }
         
--- ARGame_2_14/ViewController.swift
+++ ARGame_2_14/ViewController.swift
@@ -14,28 +14,59 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     private let boxModel = BoxModel()
+    private let colorRange = [0: #colorLiteral(red: 1, green: 0.9598860748, blue: 0.4043213171, alpha: 0.9), 1: #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 0.9), 2: #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 0.9), 3: #colorLiteral(red: 0.1411764771, green: 0.3960784376, blue: 0.5647059083, alpha: 0.9), 4: #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 0.9), 5: #colorLiteral(red: 0.6067150242, green: 1, blue: 0.9936729992, alpha: 0.9)]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Set the view's delegate
         sceneView.delegate = self
+        addBoxes()
+        addColorPanel()
+    }
+    
+    func addBoxes() {
+        let boxNodes = boxModel.generateBoxNodes(boxesCount: 100, colorRange: colorRange)
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-//        // Create a new scene
-//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-//
-//        // Set the scene to the view
-//        sceneView.scene = scene
-        
-        let boxes = boxModel.generateBoxes(boxesCount: 100)
-        
-        for box in boxes {
+        for box in boxNodes {
             sceneView.scene.rootNode.addChildNode(box)
         }
+    }
+    
+    func addColorPanel() {
+        let panel = UIView()
+        let h = view.frame.height / 8
+        panel.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.1988655822)
         
+        panel.frame = CGRect(x: 0.0, y: view.frame.height - h, width: view.frame.width, height: h)
+        
+        let offset = panel.frame.width / 7
+        var buttonX = offset
+        for i in 0..<colorRange.count {
+            let button = UIButton()
+            let w = panel.frame.height / 4
+            button.backgroundColor = colorRange[i]
+            button.frame = CGRect(x: 0, y: 0, width: w, height: w)
+            button.layer.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+            button.layer.position = CGPoint(x: buttonX, y: panel.frame.height / 2)
+            button.layer.cornerRadius = button.frame.width / 2
+            buttonX += offset
+            button.addTarget(self, action: #selector(buttonAction(sender: )), for: .touchUpInside)
+            
+            panel.addSubview(button)
+        }
+    
+        self.view.addSubview(panel)
+    }
+    
+    @objc func buttonAction(sender: UIButton) {
+        let sphere = SCNSphere(radius: 0.05)
+        sphere.firstMaterial?.diffuse.contents =  sender.backgroundColor
+        sphere.firstMaterial?.isDoubleSided = true
+        
+        let sphereNode = SCNNode(geometry: sphere)
+        sphereNode.position = SCNVector3(0, 0, -1)
+        
+        sceneView.scene.rootNode.addChildNode(sphereNode)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -56,15 +87,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
 
     // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
